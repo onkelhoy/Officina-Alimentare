8cf6087a1c8cd10e93684d8dd9efd34260a56d32
Henry Pap <onkelhoy@gmail.com>
2021-12-24 00:19:48 +0100

    remove weird src fix

--- src/ux/atoms/Image/Component.tsx
+++ src/ux/atoms/Image/Component.tsx
@@ -18,11 +18,11 @@ export const Image = React.forwardRef<HTMLImageElement, Props & IProps>((props, 
   const [loading, setLoading] = React.useState<boolean>(true);
 
   let { src } = props;
-  if (window.location.host === "onkelhoy.github.io") {
-    let slash = src[0] === '/' ? '' : '/';
-    src = `/officina_alimentare_atomic${slash}${src}`
-    console.log({ slash, src });
-  }
+  // if (window.location.host === "onkelhoy.github.io") {
+  //   let slash = src[0] === '/' ? '' : '/';
+  //   src = `/officina_alimentare_atomic${slash}${src}`
+  //   console.log({ slash, src });
+  // }
 
   const image = new window.Image();
   image.src = src;
